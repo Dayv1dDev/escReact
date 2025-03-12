d06652834e08dfaaaa4f4b289ea7554ec3e551ad
Dayv1d <davidmorecam@gmail.com>
2025-03-12 03:31:22 +0100

    Enhance NavBar styling and update Home page content layout

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -8,8 +8,8 @@ export default function NavBar() {
         <header className="absolute z-50 top-0 left-0 w-full h-20 flex items-center justify-between px-8">
             <Link to="/"><h1 className="text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-white to-[#adadad]">escReact</h1></Link>
             <nav className="flex gap-3">
-                <Link to="/esc" className="py-2 px-4 flex items-center">Esc</Link>
-                <Link to="/react" className="py-2 px-4 flex items-center">React</Link>
+                <Link to="/esc" className="py-2 px-4 font-semibold flex items-center">Esc</Link>
+                <Link to="/react" className="py-2 px-4 font-semibold flex items-center">React</Link>
                 <Link to="/login" className="py-2 px-4 flex items-center font-semibold bg-gradient-to-br from-emerald-600 to-blue-600 rounded-full">Iniciar sesión</Link>
                 <Link to="/register" className="py-2 px-4 font-semibold border border-white rounded-full hover:bg-white/10 hover:border-white/50 transition-all duration-300">Regístrate</Link>
             </nav>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,16 +3,21 @@ import NavBar from "../components/NavBar.jsx";
 
 export default function Home() {
     return (    
-        <>
+        <div className="w-screen h-screen flex flex-col">
             <MainBackground />
-            <main>
+            <main className="h-full">
                 <NavBar />
                 <section className="flex flex-col items-center">
-                    <h1 className="text-6xl font-bold">¡Bienvenido/a a escReact!</h1>
-                    <p className="text-2xl">La mejor plataforma de aprendizaje de Esc y React</p>
+                    <h1 className="text-3xl">La plataforma para prácticar mecanografía y reducir tu tiempo de reacción</h1>
                     <a href="/register.html" className="w-fit py-2 px-4 mt-4 font-semibold border border-black rounded-full hover:bg-black/10 hover:border-black/50 transition-all duration-150">Regístrate</a>
                 </section>
             </main>
-        </>
+            <footer className="flex items-center justify-center border-t border-[#333]">
+                <div className="flex flex-col w-7xl">
+                    <p>escReact &copy; 2025</p>
+                    <p>Sitio web programado por David Moreno Cámara</p>
+                </div>
+            </footer>
+        </div>
     )
 }
