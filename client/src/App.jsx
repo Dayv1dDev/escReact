fbda71f0fa9d1d46a9411344c12a1f5612b92c05
Dayv1d <davidmorecam@gmail.com>
2025-03-19 19:44:45 +0100

    Add Esc route to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,15 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
+import Esc from './pages/Esc.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      {/* <Route path="/register" element={<Register />} />
-      <Route path="/esc" element={<Esc />} /> */}
+      {/* <Route path="/register" element={<Register />} /> */}
+      <Route path="/esc" element={<Esc />} />
     </Routes>
   )
 }
