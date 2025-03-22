bee58d98a9db738896832f2d50b078363684a254
Dayv1d <davidmorecam@gmail.com>
2025-03-22 03:58:48 +0100

    Add input handling and statistics display to Esc component

--- client/src/pages/Esc.jsx
+++ client/src/pages/Esc.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { act, useEffect, useRef, useState } from "react";
 import { words } from "../data/words";
 import MainBackground from "../components/MainBackground";
 
 export default function Esc() {
+    const inputRef = useRef(null);
+
     const INITIAL_TIME = 30;
     const sortedWords = words.sort(() => Math.random() - 0.5).slice(0, 30);
     const [time, setTime] = useState(INITIAL_TIME);
@@ -10,7 +12,7 @@ export default function Esc() {
 
     useEffect(() => {
         startGame();
-        // startEvents();
+        startEvents();
     }, []);
 
     function startGame() {
@@ -37,13 +39,100 @@ export default function Esc() {
     }
     
     function startEvents() {
-        // document.addEventListener("keydown", handleKeyDown);
+        document.addEventListener("keydown", () => {
+            inputRef.current.focus();
+        });
+
+        inputRef.current.addEventListener("keydown", keyDown);
+        inputRef.current.addEventListener("keyup", keyUp);
     }
 
-    function handleKeyDown(event) {
-        console.log(event.key);
-    }
+    function keyDown(e) {
+        const { key } = e;
+        const activeWordEl = document.querySelector("x-word.active");
+        const activeLetterEl = activeWordEl.querySelector("x-letter.active");
 
+        if (key === " ") {
+            e.preventDefault();
+            
+            const nextWordEl = activeWordEl.nextElementSibling;
+            const nextLetterEl = nextWordEl.querySelector("x-letter");
+
+            activeWordEl.classList.remove("active", "marked");
+            activeLetterEl.classList.remove("active");
+
+            nextWordEl.classList.add("active");
+            nextLetterEl.classList.add("active");
+
+            inputRef.current.value = "";
+
+            const hasMissingLetters = activeWordEl.querySelectorAll("x-letter:not(.correct)").length > 0;
+            const classToAdd = hasMissingLetters ? "marked" : "correct";
+            activeWordEl.classList.add(classToAdd);
+        }
+
+        if (key === "Backspace") {
+            const prevWordEl = activeWordEl.previousElementSibling;
+            const prevLetterEl = activeLetterEl.previousElementSibling;
+
+            if (!prevWordEl && !prevLetterEl) {
+                e.preventDefault();
+                return;
+            }
+            
+            const markedWordEl = document.querySelector("x-word.marked");
+            if (!prevLetterEl && markedWordEl) {
+                e.preventDefault();
+
+                activeWordEl.classList.remove("active");
+                activeLetterEl.classList.remove("active");
+                prevWordEl.classList.remove("marked");
+                prevWordEl.classList.add("active");
+
+                const destinationLetterEl = prevWordEl.querySelector("x-letter:last-child");
+                destinationLetterEl.classList.add("active");
+
+                inputRef.current.value = [
+                    ...prevWordEl.querySelectorAll("x-letter.correct", "x-letter.incorrect")
+                ].map(letterEl => { 
+                    return letterEl.classList.contains("correct") ? letterEl.innerText : "?";
+                }).join("");
+            }
+        }
+    }
+    
+
+    function keyUp(e) {
+        const activeWordEl = document.querySelector("x-word.active");
+        const activeLetterEl = activeWordEl.querySelector("x-letter.active");
+        const lettersEl = activeWordEl.querySelectorAll("x-letter");
+
+        const currentWord = activeWordEl.textContent.trim();
+        inputRef.current.maxLength = currentWord.length;
+
+
+        lettersEl.forEach(letterEl => {
+            letterEl.classList.remove("correct", "incorrect");
+        });
+        
+        inputRef.current.value.split("").forEach((char, index) => {
+            const letterEl = lettersEl[index];
+            const letterToCheck = currentWord[index];
+            
+            const isCorrect = char === letterToCheck;
+            const letterClass = isCorrect ? "correct" : "incorrect";
+            
+            letterEl.classList.add(letterClass);
+        })
+
+        activeLetterEl.classList.remove("active", "is-last");
+        const inputLength = inputRef.current.value.length;
+        const nextLetterEl = lettersEl[inputLength];
+
+        if (nextLetterEl) nextLetterEl.classList.add("active");  
+        else activeLetterEl.classList.add("active", "is-last");
+        // TO-DO: Revisar que la clase is-last funcione correctamente
+    }
 
     return (
         <main className="h-screen flex justify-center items-center font-mono">
@@ -65,7 +154,14 @@ export default function Esc() {
                         )
                     })}
                 </p>
-                <input autoFocus className="absolute top-0 left-0 pointer-events-none" />
+                <input ref={inputRef} autoFocus className="absolute top-0 left-0 pointer-events-none" />
+            </section>
+            <section id="stats" className="hidden">
+                <h2>Resultados:</h2>
+                <p>Palabras correctas: <span id="correct-words">0</span></p>
+                <p>Palabras incorrectas: <span id="incorrect-words">0</span></p>
+                <p>Palabras totales: <span id="total-words">0</span></p>
+                <p>Velocidad: <span id="speed">0</span> ppm</p>
             </section>
         </main>
     )
