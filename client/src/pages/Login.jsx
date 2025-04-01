efcdec395d4e1d236cc627150f836db6625bc99e
Dayv1d <davidmorecam@gmail.com>
2025-04-01 02:12:46 +0200

    Implement user login and registration functionality with Axios integration

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,30 @@
-import { Link } from "react-router-dom"
-import "../App.css"
-import MainBackground from "../components/MainBackground"
+import "../App.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Axios from "axios";
+import MainBackground from "../components/MainBackground";
 
 export default function Login() { 
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [loginStatus, setLoginStatus] = useState("");
+
+    function loginUser(e) {
+        e.preventDefault();
+
+        Axios.post("http://localhost:5713/login", {
+            username: username,
+            password: password
+        }).then((res) => {
+            if (res.data.message) {
+                setLoginStatus(res.data.message);
+            } else {
+                setLoginStatus(res.data[0].username);
+            }
+        })
+    }
+    
     return (
         <>
             <MainBackground />
@@ -11,22 +33,23 @@ export default function Login() {
                     <form method="POST" action="/login" className="flex flex-col justify-center w-full rounded-l-sm bg-black/50 p-10">
                         <h1 className="text-3xl self-center font-bold mb-9">Inicia sesión en escReact</h1>
                         <div className="relative">
-                            <input className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='text' id='username' name='username' required />
+                            <input onChange={(e) => {setUsername(e.target.value)}} className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='text' id='username' name='username' maxLength={55} required />
                             <label className="font-semibold absolute left-0 pointer-events-none py-2 pr-1 pl-1 text-base transition-all duration-300" htmlFor="username">Nombre de usuario</label>
                             <i className="absolute left-0 bottom-0 w-full h-0.5 bg-gradient-to-br from-emerald-600 to-blue-600 overflow-hidden rounded-xs transition-all duration-300"></i>
                             <div>
-                                <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/40 overflow-hidden transition-all duration-300"></i>
+                                <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/80 overflow-hidden transition-all duration-300"></i>
                             </div>
                         </div>
                         <div className="mt-8 mb-2 relative">
-                            <input className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='password' id='password' name='password' required />
+                            <input onChange={(e) => {setPassword(e.target.value)}} className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='password' id='password' name='password' maxLength={25} required />
                             <label className="font-semibold absolute left-0 pointer-events-none py-2 pr-1 pl-1 text-base transition-all duration-300" htmlFor="password">Contraseña</label>
                             <i className="absolute left-0 bottom-0 w-full h-0.5 bg-gradient-to-br from-emerald-600 to-blue-600 overflow-hidden rounded-xs transition-all duration-300"></i>
                             <div>
-                                <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/40 overflow-hidden transition-all duration-300"></i>
+                                <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/80 overflow-hidden transition-all duration-300"></i>
                             </div>
                         </div>
-                        <button type="submit" className="w-fit self-center py-2 px-8 mt-4 font-semibold rounded-full bg-gradient-to-br from-emerald-600 to-blue-600 hover:scale-110 transition-all duration-150">Iniciar sesión</button>
+                        <h1>{loginStatus}</h1>
+                        <button onClick={loginUser} type="submit" className="w-fit self-center py-2 px-8 mt-4 font-semibold rounded-full bg-gradient-to-br from-emerald-600 to-blue-600 hover:scale-110 transition-all duration-150">Iniciar sesión</button>
                     </form>
                     <aside className="flex flex-col items-center justify-center gap-3 p-10 w-full rounded-r-sm bg-gradient-to-br from-emerald-600 to-blue-600">
                         <h1 className="text-3xl font-bold text-center">¿Eres nuevo/a?</h1>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,26 @@
 import "../App.css";
 import { Link } from "react-router-dom";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import Axios from "axios";
 import MainBackground from "../components/MainBackground";
 
 export default function Register() {
     const emailInputRef = useRef(null);
 
-    let isRegistered = false;
-    if (isRegistered) {
-        window.location.href = "/";
+    const [regUsername, setRegUsername] = useState("");
+    const [regEmail, setRegEmail] = useState("");
+    const [regPassword, setRegPassword] = useState("");
+
+    function registerUser() {
+        Axios.post("http://localhost:5713/register", {
+            username: regUsername,
+            email: regEmail,
+            password: regPassword
+        }).then((res) => {
+            console.log(res.data);
+        }).catch((err) => {
+            console.error("Error al registrar el usuario: ", err);
+        });
     }
 
     function handleEmailInput() {
@@ -31,7 +43,6 @@ export default function Register() {
         return
     }
 
-
     return (
         <>
             <MainBackground />
@@ -46,31 +57,31 @@ export default function Register() {
                         <h1 className="text-3xl self-center font-bold mb-9">Crea tu cuenta de escReact</h1>
                         <article className="flex flex-col gap-9 mb-4">
                             <div className="relative">
-                                <input className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='text' id='username' name='username' required />
+                                <input onChange={(e) => {setRegUsername(e.target.value)}} className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='text' id='username' name='username' maxLength={55} required />
                                 <label className="font-semibold absolute left-0 pointer-events-none py-2 pr-1 pl-1 text-base transition-all duration-300" htmlFor="username">Nombre de usuario</label>
                                 <i className="absolute left-0 bottom-0 w-full h-0.5 bg-gradient-to-br from-emerald-600 to-blue-600 overflow-hidden rounded-xs transition-all duration-300"></i>
                                 <div>
-                                    <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/40 overflow-hidden transition-all duration-300"></i>
+                                    <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/80 overflow-hidden transition-all duration-300"></i>
                                 </div>
                             </div>
                             <div className="relative">
-                                <input ref={emailInputRef} onInput={handleEmailInput} className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type="email" id='email' name='email' required />
+                                <input ref={emailInputRef} onInput={handleEmailInput} onChange={(e) => {setRegEmail(e.target.value)}} className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type="email" id='email' name='email' maxLength={100} required />
                                 <label className="font-semibold absolute left-0 pointer-events-none py-2 pr-1 pl-1 text-base transition-all duration-300" htmlFor="email">Correo electrónico</label>
                                 <i className="absolute left-0 bottom-0 w-full h-0.5 bg-gradient-to-br from-emerald-600 to-blue-600 overflow-hidden rounded-xs transition-all duration-300"></i>
                                 <div>
-                                    <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/40 overflow-hidden transition-all duration-300"></i>
+                                    <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/80 overflow-hidden transition-all duration-300"></i>
                                 </div>
                             </div>
                             <div className="relative">
-                                <input className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='password' id='password' name='password' required />
+                                <input onChange={(e) => {setRegPassword(e.target.value)}} className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='password' id='password' name='password' maxLength={25} required />
                                 <label className="font-semibold absolute left-0 pointer-events-none py-2 pr-1 pl-1 text-base transition-all duration-300" htmlFor="password">Contraseña</label>
                                 <i className="absolute left-0 bottom-0 w-full h-0.5 bg-gradient-to-br from-emerald-600 to-blue-600 overflow-hidden rounded-xs transition-all duration-300"></i>
                                 <div>
-                                    <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/40 overflow-hidden transition-all duration-300"></i>
+                                    <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/80 overflow-hidden transition-all duration-300"></i>
                                 </div>
                             </div>
                         </article>
-                        <button type="submit" className="w-fit self-center py-2 px-8 mt-4 font-semibold rounded-full bg-gradient-to-br from-emerald-600 to-blue-600 hover:scale-110 transition-all duration-150">Registrarse</button>
+                        <button onClick={registerUser} type="submit" className="w-fit self-center py-2 px-8 mt-4 font-semibold rounded-full bg-gradient-to-br from-emerald-600 to-blue-600 hover:scale-110 transition-all duration-150">Registrarse</button>
                     </form>
                 </section>
             </main>
