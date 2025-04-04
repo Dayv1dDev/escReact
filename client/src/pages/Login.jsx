5ea26e5312eb53346eba2bb8fd8aaf8191efe8f5
Dayv1d <davidmorecam@gmail.com>
2025-04-04 03:16:43 +0200

    Enhance login functionality by adding session check and updating API endpoint

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import "../App.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Axios from "axios";
 import MainBackground from "../components/MainBackground";
 
@@ -10,10 +10,12 @@ export default function Login() {
 
     const [loginStatus, setLoginStatus] = useState("");
 
+    Axios.defaults.withCredentials = true;
+
     function loginUser(e) {
         e.preventDefault();
 
-        Axios.post("http://localhost:5713/login", {
+        Axios.post("http://localhost:5174/login", {
             username: username,
             password: password
         }).then((res) => {
@@ -24,6 +26,13 @@ export default function Login() {
             }
         })
     }
+
+    useEffect(() => {
+        Axios.get("http://localhost:5174/login").then((res) => {
+            console.log(res)
+            if (res.data.isLoggedIn === true) setLoginStatus(res.data.user[0].username);
+        });
+    }, []);
     
     return (
         <>
@@ -34,7 +43,7 @@ export default function Login() {
                         <h1 className="text-3xl self-center font-bold mb-9">Inicia sesión en escReact</h1>
                         <div className="relative">
                             <input onChange={(e) => {setUsername(e.target.value)}} className="relative w-full py-2 pr-2 pl-2 bg-transparent text-base font-medium border-none outline-0 z-20 transition-all duration-300" type='text' id='username' name='username' maxLength={55} required />
-                            <label className="font-semibold absolute left-0 pointer-events-none py-2 pr-1 pl-1 text-base transition-all duration-300" htmlFor="username">Nombre de usuario</label>
+                            <label className="font-semibold absolute left-0 pointer-events-none py-2 pr-1 pl-1 text-base transition-all duration-300" htmlFor="username">Usuario</label>
                             <i className="absolute left-0 bottom-0 w-full h-0.5 bg-gradient-to-br from-emerald-600 to-blue-600 overflow-hidden rounded-xs transition-all duration-300"></i>
                             <div>
                                 <i className="absolute left-0.5 bottom-0.5 w-[calc(100%-4px)] h-0 z-10 opacity-0 bg-black/80 overflow-hidden transition-all duration-300"></i>
