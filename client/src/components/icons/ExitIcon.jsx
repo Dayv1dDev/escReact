e9a8b1f81a7d55e875817244734edbae2b579ce0
Dayv1d <davidmorecam@gmail.com>
2025-04-05 02:34:37 +0200

    Add ExitIcon and UserIcon components for enhanced UI

---
+++ client/src/components/icons/ExitIcon.jsx
@@ -0,0 +1,9 @@
+export default function ExitIcon({className}) {
+    return (
+        <svg className={`${className}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" width="24" height="24" strokeWidth="1.25">
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
+            <path d="M9 12h12l-3 -3"></path>
+            <path d="M18 15l3 -3"></path>
+        </svg>
+    )
+}
