202e48ab7b2af5277afb268b9a50fd37017f60a8
Dayv1d <davidmorecam@gmail.com>
2025-04-19 18:36:45 +0200

    Enhance NavLink component: add animation effect on hover for improved user interaction

--- client/src/components/NavLink.jsx
+++ client/src/components/NavLink.jsx
@@ -3,6 +3,6 @@ import { Link } from "react-router-dom";
 
 export default function NavLink({href, text}) {
     return (
-        <Link to={href} className="py-1.5 px-3 font-semibold flex items-center nav-link-animation">{text}</Link>
+        <Link to={href} className="relative nav-link-animation-parent font-semibold flex items-center"><span className="w-fit nav-link-animation">{text}</span></Link>
     )
 }
